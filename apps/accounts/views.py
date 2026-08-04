3af70d6e215d6ff55557f5a3e0f0210209a4cfcd
danik322 <danylobucharov@gmail.com>
2026-08-04 20:41:48 +0300

    moved tokens to get_tokens function, made and patch func for user profile. Added public profile viewing, and changing/viewing only ur profile

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,9 +3,11 @@ from rest_framework import routers, viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
+from .models import CustomUser
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
@@ -16,6 +18,14 @@ class RegisterView(APIView):
             serializer.save()
             return Response({"message":"User successfully created!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def get_tokens(user):
+    refresh  = RefreshToken.for_user(user)
+    return {
+        "access": str(refresh.access_token),
+        "refresh": str(refresh),
+    }
 
 class LoginView(APIView):
     permission_classes = [AllowAny]
@@ -29,17 +39,29 @@ class LoginView(APIView):
                 password=serializer.validated_data['password']
             )
             if not user:
-                return Response({"error": "Invalid credentials!"}, status=status.HTTP_401_UNAUTHORIZED)
-
-            refresh  = RefreshToken.for_user(user)
+                return Response({"detail": "Invalid credentials!"}, status=status.HTTP_401_UNAUTHORIZED)
 
             return Response({"message": "Logged in!",
-                            "access": str(refresh.access_token),
-                            "refresh": str(refresh),}, status=status.HTTP_200_OK)
+                            "tokens": get_tokens(user)}, status=status.HTTP_200_OK)
 
-class ProfileView(APIView):
-
+class MeView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user) 
+        return Response(serializer.data)
+
+    def patch(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, username):
+        user = get_object_or_404(CustomUser, username=username)
+        serializer = UserSerializer(user)
         return Response(serializer.data)
 # Create your views here.
