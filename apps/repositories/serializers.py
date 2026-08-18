7a2936dcf4ae18dcf70be03c47a98b66315c543a
danik322 <danylobucharov@gmail.com>
2026-08-18 16:12:05 +0300

    typo

--- apps/repositories/serializers.py
+++ apps/repositories/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Repository, Star, Collaboratorw
+from .models import Repository, Star, Collaborator
 
 
 class RepositorySerializer(serializers.ModelSerializer):
