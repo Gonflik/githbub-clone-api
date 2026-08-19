d9f1b7b517bca1fb03be014de4d4181f5e44ad5f
danik322 <danylobucharov@gmail.com>
2026-08-19 16:27:38 +0300

    Added visibility attribute and class, changed Roles to only Owner and memer

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Organization(models.Model):
     org_name = models.CharField(max_length=40, unique=True)
-    display_name = models.CharField(max_length=100)
+    display_name = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
 
     created_by = models.ForeignKey("accounts.CustomUser", on_delete=models.SET_NULL, null=True, related_name="created_organizations")
@@ -14,11 +14,14 @@ class Organization(models.Model):
 class OrgMember(models.Model):
     class Role(models.TextChoices):
         OWNER = "OWNER", "Owner"
-        ADMIN = "ADMIN", "Admin"
         MEMBER = "MEMBER", "Member"
-        GUEST = "GUEST", "Guest"
+
+    class Visibility(models.TextChoices):
+        PUBLIC = "PUBLIC", "Public"
+        PRIVATE = "PRIVATE", "Private"
 
     role = models.CharField(max_length=6, choices=Role.choices, default=Role.MEMBER)
+    visibility = models.CharField(max_length=7, choices=Visibility.choices, default=Visibility.PRIVATE)
 
     user = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name="org_memberships")
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="members")
