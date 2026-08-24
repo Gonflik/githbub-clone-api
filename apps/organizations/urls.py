cbeb3206102992920f55dc110b8a79f4583d1f14
danik322 <danylobucharov@gmail.com>
2026-08-24 18:06:51 +0300

    urls

--- apps/organizations/urls.py
+++ apps/organizations/urls.py
@@ -1,9 +1,12 @@
 from rest_framework_nested import routers
-from .views import OrganizationViewSet
+from .views import OrganizationViewSet, OrgMemberViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r"organizations", OrganizationViewSet, basename="organization")
+
+org_member_router = routers.NestedDefaultRouter(router, "organizations", lookup="organization")
+org_member_router.register("members", OrgMemberViewSet, basename="organization-members")
 
 
 urlpatterns = [
